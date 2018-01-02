3c600d2d143f3c208ed64631483c38cb58d95d18
Ahmed Al Ansaary <ahansaary@gmail.com>
2018-01-02 15:02:53 +0200

    Some Enhancements in connection listen method

--- src/services/connection/signalr.connection.ts
+++ src/services/connection/signalr.connection.ts
@@ -1,178 +1,227 @@
-import { ISignalRConnection } from './i.signalr.connection';
-import { Observable } from 'rxjs/Observable';
-import { BroadcastEventListener } from '../eventing/broadcast.event.listener';
-import { ConnectionStatus } from './connection.status';
-import { NgZone } from '@angular/core';
-import { Subject } from 'rxjs/Subject';
-import { SignalRConfiguration } from '../signalr.configuration';
-import { ConnectionTransport } from './connection.transport';
+import { ISignalRConnection } from './i.signalr.connection'
+import { Observable } from 'rxjs/Observable'
+import { BroadcastEventListener } from '../eventing/broadcast.event.listener'
+import { ConnectionStatus } from './connection.status'
+import { NgZone } from '@angular/core'
+import { Subject } from 'rxjs/Subject'
+import { SignalRConfiguration } from '../signalr.configuration'
+import { ConnectionTransport } from './connection.transport'
+
+export declare type CallbackFn = (...args: any[]) => void
 
 export class SignalRConnection implements ISignalRConnection {
-    private _status: Observable<ConnectionStatus>;
-    private _errors: Observable<any>;
-    private _jConnection: any;
-    private _jProxy: any;
-    private _zone: NgZone;
-    private _configuration: SignalRConfiguration;
+  private _status: Observable<ConnectionStatus>
+  private _errors: Observable<any>
+  private _jConnection: any
+  private _jProxy: any
+  private _zone: NgZone
+  private _configuration: SignalRConfiguration
+  private _listeners: { [eventName: string]: CallbackFn[] }
 
-    constructor(jConnection: any, jProxy: any, zone: NgZone, configuration: SignalRConfiguration) {
-        this._jProxy = jProxy;
-        this._jConnection = jConnection;
-        this._zone = zone;
-        this._errors = this.wireUpErrorsAsObservable();
-        this._status = this.wireUpStatusEventsAsObservable();
-        this._configuration = configuration;
+  constructor(
+    jConnection: any,
+    jProxy: any,
+    zone: NgZone,
+    configuration: SignalRConfiguration
+  ) {
+    this._jProxy = jProxy
+    this._jConnection = jConnection
+    this._zone = zone
+    this._errors = this.wireUpErrorsAsObservable()
+    this._status = this.wireUpStatusEventsAsObservable()
+    this._configuration = configuration
+    this._listeners = {}
+  }
+
+  public get errors(): Observable<any> {
+    return this._errors
+  }
+
+  public get status(): Observable<ConnectionStatus> {
+    return this._status
+  }
+
+  public start(): Promise<ISignalRConnection> {
+    let jTransports = this.convertTransports(this._configuration.transport)
+
+    let $promise = new Promise<ISignalRConnection>((resolve, reject) => {
+      this._jConnection
+        .start({
+          jsonp: this._configuration.jsonp,
+          transport: jTransports,
+          withCredentials: this._configuration.withCredentials
+        })
+        .done(() => {
+          console.log('Connection established, ID: ' + this._jConnection.id)
+          console.log(
+            'Connection established, Transport: ' +
+              this._jConnection.transport.name
+          )
+          resolve(this)
+        })
+        .fail((error: any) => {
+          console.log('Could not connect')
+          reject('Failed to connect. Error: ' + error.message) // ex: Error during negotiation request.
+        })
+    })
+    return $promise
+  }
+
+  public stop(): void {
+    this._jConnection.stop()
+  }
+
+  public get id(): string {
+    return this._jConnection.id
+  }
+
+  public invoke(method: string, ...parameters: any[]): Promise<any> {
+    if (method == null) {
+      throw new Error(
+        "SignalRConnection: Failed to invoke. Argument 'method' can not be null"
+      )
+    }
+    this.log(`SignalRConnection. Start invoking \'${method}\'...`)
+
+    let $promise = new Promise<any>((resolve, reject) => {
+      this._jProxy
+        .invoke(method, ...parameters)
+        .done((result: any) => {
+          this.log(`\'${method}\' invoked succesfully. Resolving promise...`)
+          resolve(result)
+          this.log(`Promise resolved.`)
+        })
+        .fail((err: any) => {
+          console.log(`Invoking \'${method}\' failed. Rejecting promise...`)
+          reject(err)
+          console.log(`Promise rejected.`)
+        })
+    })
+    return $promise
+  }
+
+  public listen<T>(listener: BroadcastEventListener<T | any[]>): void {
+    if (listener == null) {
+      throw new Error("Failed to listen. Argument 'listener' can not be null")
     }
 
-    public get errors(): Observable<any> {
-        return this._errors;
-    }
-
-    public get status(): Observable<ConnectionStatus> {
-        return this._status;
-    }
-
-    public start(): Promise<ISignalRConnection> {
-
-        let jTransports = this.convertTransports(this._configuration.transport);
-
-        let $promise = new Promise<ISignalRConnection>((resolve, reject) => {
-                this._jConnection
-                .start({
-                        jsonp: this._configuration.jsonp,
-                        pingInterval: this._configuration.pingInterval,
-                        transport: jTransports,
-                        withCredentials: this._configuration.withCredentials,
-                    })
-                .done(() => {
-                        console.log('Connection established, ID: ' + this._jConnection.id);
-                        console.log('Connection established, Transport: ' + this._jConnection.transport.name);
-                        resolve(this);
-                    })
-                .fail((error: any) => {
-                        console.log('Could not connect');
-                        reject('Failed to connect. Error: ' + error.message); // ex: Error during negotiation request.
-                    });
-            });
-        return $promise;
-    }
-
-    public stop(): void {
-        this._jConnection.stop();
-    }
-
-    public get id(): string {
-        return this._jConnection.id;
-    }
-
-    public invoke(method: string, ...parameters: any[]): Promise<any> {
-        if (method == null) {
-            throw new Error('SignalRConnection: Failed to invoke. Argument \'method\' can not be null');
+    let callback: CallbackFn = (...args: any[]) => {
+      this._zone.run(() => {
+        let casted: T | any[] = null
+        if (args.length === 1) {
+          casted = <T>args[0]
+        } else if (args.length > 1) {
+          casted = args
         }
-        this.log(`SignalRConnection. Start invoking \'${method}\'...`);
-
-        let $promise = new Promise<any>((resolve, reject) => {
-            this._jProxy.invoke(method, ...parameters)
-                .done((result: any) => {
-                    this.log(`\'${method}\' invoked succesfully. Resolving promise...`);
-                    resolve(result);
-                    this.log(`Promise resolved.`);
-                })
-                .fail((err: any) => {
-                    console.log(`Invoking \'${method}\' failed. Rejecting promise...`);
-                    reject(err);
-                    console.log(`Promise rejected.`);
-                });
-        });
-        return $promise;
+        this.log(
+          'SignalRConnection.proxy.on invoked. Calling listener next() ...'
+        )
+        listener.next(casted)
+        this.log('listener next() called.')
+      })
     }
 
-    public listen<T>(listener: BroadcastEventListener<T>): void {
-        if (listener == null) {
-            throw new Error('Failed to listen. Argument \'listener\' can not be null');
-        }
+    this.log(
+      `SignalRConnection: Starting to listen to server event with name ${
+        listener.event
+      }`
+    )
+    this._jProxy.on(listener.event, callback)
 
-        this.log(`SignalRConnection: Starting to listen to server event with name ${listener.event}`);
-        this._jProxy.on(listener.event, (...args: any[]) => {
-
-            this.run(() => {
-                let casted: T = null;
-                if (args.length > 0) {
-                    casted = <T> args[0];
-                }
-                this.log('SignalRConnection.proxy.on invoked. Calling listener next() ...');
-                listener.next(casted);
-                this.log('listener next() called.');
-            }, this._configuration.executeEventsInZone);
-        });
+    if (this._listeners[listener.event] == null) {
+      this._listeners[listener.event] = []
     }
 
-    public listenFor<T>(event: string): BroadcastEventListener<T> {
-        if (event == null || event === '') {
-            throw new Error('Failed to listen. Argument \'event\' can not be empty');
-        }
+    this._listeners[listener.event].push(callback)
+  }
 
-        let listener = new BroadcastEventListener<T>(event);
-
-        this.listen(listener);
-
-        return listener;
+  public stopListening<T>(listener: BroadcastEventListener<T>): void {
+    if (listener == null) {
+      throw new Error("Failed to listen. Argument 'listener' can not be null")
     }
 
-    private convertTransports(transports: ConnectionTransport | ConnectionTransport[]): any {
-        if (transports instanceof Array) {
-            return transports.map((t: ConnectionTransport) => t.name);
-        }
-        return transports.name;
+    this.log(
+      `SignalRConnection: Stopping listening to server event with name ${
+        listener.event
+      }`
+    )
+    if (!this._listeners[listener.event]) {
+      this._listeners[listener.event] = []
     }
 
-    private wireUpErrorsAsObservable(): Observable<any> {
-        let sError = new Subject<any>();
-
-        this._jConnection.error((error: any) => {
-            this.run(() => sError.next(error), this._configuration.executeErrorsInZone);
-        });
-        return sError;
+    for (let callback of this._listeners[listener.event]) {
+      this._jProxy.off(listener.event, callback)
     }
 
-    private wireUpStatusEventsAsObservable(): Observable<ConnectionStatus> {
-        let sStatus = new Subject<ConnectionStatus>();
-        // aggregate all signalr connection status handlers into 1 observable.
-        // handler wire up, for signalr connection status callback.
-        this._jConnection.stateChanged((change: any) => {
-            this.run(() => sStatus.next(new ConnectionStatus(change.newState)),
-                                    this._configuration.executeStatusChangeInZone);
-        });
-        return sStatus.asObservable();
+    this._listeners[listener.event] = []
+  }
+
+  public listenFor<T>(event: string): BroadcastEventListener<T> {
+    if (event == null || event === '') {
+      throw new Error("Failed to listen. Argument 'event' can not be empty")
     }
 
-    private onBroadcastEventReceived<T>(listener: BroadcastEventListener<T>, ...args: any[]) {
-        this.log('SignalRConnection.proxy.on invoked. Calling listener next() ...');
+    let listener = new BroadcastEventListener<T>(event)
 
-        let casted: T = null;
-        if (args.length > 0) {
-            casted = <T> args[0];
-        }
+    this.listen(listener)
 
-        this.run(() => {
-            listener.next(casted);
-        }, this._configuration.executeEventsInZone);
+    return listener
+  }
 
-        this.log('listener next() called.');
+  private convertTransports(
+    transports: ConnectionTransport | ConnectionTransport[]
+  ): any {
+    if (transports instanceof Array) {
+      return transports.map((t: ConnectionTransport) => t.name)
+    }
+    return transports.name
+  }
+
+  private wireUpErrorsAsObservable(): Observable<any> {
+    let sError = new Subject<any>()
+
+    this._jConnection.error((error: any) => {
+      //this._zone.run(() => {  /*errors don't need to run in a  zone*/
+      sError.next(error)
+      //});
+    })
+    return sError
+  }
+
+  private wireUpStatusEventsAsObservable(): Observable<ConnectionStatus> {
+    let sStatus = new Subject<ConnectionStatus>()
+    // aggregate all signalr connection status handlers into 1 observable.
+    // handler wire up, for signalr connection status callback.
+    this._jConnection.stateChanged((change: any) => {
+      this._zone.run(() => {
+        sStatus.next(new ConnectionStatus(change.newState))
+      })
+    })
+    return sStatus.asObservable()
+  }
+
+  private onBroadcastEventReceived<T>(
+    listener: BroadcastEventListener<T>,
+    ...args: any[]
+  ) {
+    this.log('SignalRConnection.proxy.on invoked. Calling listener next() ...')
+
+    let casted: T = null
+    if (args.length > 0) {
+      casted = <T>args[0]
     }
 
-    private log(...args: any[]) {
-        if (this._jConnection.logging === false) {
-            return;
-        }
-        console.log(args.join(', '));
-    }
+    this._zone.run(() => {
+      listener.next(casted)
+    })
 
-    private run(func: Function, inZone: boolean) {
-        if (inZone) {
-            this._zone.run(() => func());
-        }else {
-            this._zone.runOutsideAngular(() => func());
-        }
+    this.log('listener next() called.')
+  }
+
+  private log(...args: any[]) {
+    if (this._jConnection.logging === false) {
+      return
     }
+    console.log('[SignalR]', args.join(', '))
+  }
 }
